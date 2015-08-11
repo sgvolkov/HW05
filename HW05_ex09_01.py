2f4013c98bc46951b3e67b63051a6f9cb22141d1
‘sgvolkov’ <svolkov@ischool.berkeley.edu>
2015-08-10 22:18:01 -0700

    adding HW05 chap 9 ex 01

--- HW05_ex09_01.py
+++ HW05_ex09_01.py
@@ -4,14 +4,19 @@
 # Write a program that reads words.txt and prints only the 
 # words with more than 20 characters (not counting whitespace).
 ##############################################################################
-# Imports
 
-# Body
+text = open('words.txt')
 
+def words(text):
+    for word in text:
+        if len(word) > 20:
+            print word.rstrip('\r\n')
+        else:
+            continue
 
 ##############################################################################
 def main():
-    pass # Call your functions here.
+    words(text)
 
 if __name__ == '__main__':
     main()
