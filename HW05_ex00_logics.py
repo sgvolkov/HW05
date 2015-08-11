8172d7438f4a8768987ded37210fd9227fc2e63f
‘sgvolkov’ <svolkov@ischool.berkeley.edu>
2015-08-10 20:09:01 -0700

    add ex00 HW05

--- HW05_ex00_logics.py
+++ HW05_ex00_logics.py
@@ -1,38 +1,52 @@
 #!/usr/bin/env python
 # HW05_ex00_logics.py
 ##############################################################################
+from __future__ import print_function
 
 def even_odd():
-    """ Print even or odd:
-        Takes one integer from user
-            accepts only non-word numerals
-            must validate
-        Determines if even or odd
-        Prints determination
-        returns None
-    """
+    while True:
+        num = raw_input('> ')
+        try:
+            num = int(num)
+            break
+        except:
+            print ("That's not a number, try again!")
+    if num % 2 == 0:
+        print ("That's an even number")
+    else:
+        print ("That's an odd number")
     pass
-
 
 def ten_by_ten():
-    """ Prints integers 1 through 100 sequentially in a ten by ten grid."""
-    pass
-
+    for x in range (1, 11):
+        for y in range (1, 11):
+            print ('{:3}'.format(x*y), end=' ') #This is a 10x10 table w/ for loops
+    print()
 
 def find_average():
-    """ Takes numeric input (non-word numerals) from the user, one number
-    at a time. Once the user types 'done', returns the average.
-    """
-    pass
+    total = 0
+    numbers = 0
+    while True:
+        num = raw_input('Enter a number, or done when finished: ')
+        try:
+            num_int = int(num)
+        except:
+            if num == 'done':
+                break
+            else:
+                print ("Try using a number")
+        if num_int > 0:
+            value = float(num_int)
+            total = total + value
+            numbers = numbers + 1
+            average = total / numbers
+            print ('Average: ', average)
 
 ##############################################################################
 def main():
-    """ Calls the following functions:
-        - even_odd()
-        - ten_by_ten()
-    Prints the following function:
-        - find_average()
-    """
+    even_odd()
+    ten_by_ten()
+    find_average()
     pass
 
 if __name__ == '__main__':
